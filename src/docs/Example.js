0e2e9fa0d3498c89ef0346197d88659eef6854b7
Your Name <you@example.com>
2017-06-24 02:32:38 +0900

    CodeExampleはcodeをhighlightしたComponentです。 ExampleHelloWorldはHelloWorld Componentで../src/components/HelloWorld/HelloWorld.jsをimportしています。 ExampleComponentはCodeExample, ExampleHelloWorldをimportまたはrequireします。

--- src/docs/Example.js
+++ src/docs/Example.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CodeExample from './CodeExample';
 
 class Example extends React.Component {
   constructor(props) {
@@ -18,12 +19,13 @@ class Example extends React.Component {
     const {showCode} = this.state;
     const {code, description, name} = this.props.example;
     // Must use CommonJS require to dynamically require because ES Modules must be statically analyzable.
-    //const ExampleComponent = require(`./examples/${this.props.componentName}/${name}`).default;
+    const ExampleComponent = require(
+      `./examples/${this.props.componentName}/${name}`).default;
     return (
       <div className="example">
         {description && <h4>{description}</h4> }
 
-        
+        <ExampleComponent />
 
         <p>
           <a href="#" onClick={this.toggleCode}>
@@ -31,7 +33,7 @@ class Example extends React.Component {
           </a>
         </p>
 
-        {showCode && code}
+        {showCode && <CodeExample>{code}</CodeExample>}
       </div>
     )
   }
