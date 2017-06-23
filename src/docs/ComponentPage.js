33ce340a1c8e338b8cad8502f7b73008fcaf5c6c
Your Name <you@example.com>
2017-06-24 02:15:11 +0900

    We changed lines and paragraphs to make it easier to read.

--- src/docs/ComponentPage.js
+++ src/docs/ComponentPage.js
@@ -4,17 +4,30 @@ import Example from './Example';
 import Props from './Props';
 
 const ComponentPage = ({component}) => {
-  const {name, description, props, examples} = component;
+  const {
+    name,
+    description,
+    props,
+    examples
+  } = component;
 
   return (
     <div className="componentpage">
       <h2>{name}</h2>
       <p>{description}</p>
-
-      <h3>Example{examples.length > 1 && "s"}</h3>
+      <h3>
+        Example{examples.length > 1 && "s"}
+      </h3>
       {
         examples.length > 0 ?
-        examples.map( example => <Example key={example.name} example={example} componentName={name} /> ) :
+        examples.map( example =>
+          <Example
+            key={example.name}
+            example={example}
+            componentName={name}
+          />
+        )
+        :
         "No examples exist."
       }
 
