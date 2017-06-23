df3f6e61037e8d4f73c22cee901b5414a1458756
Your Name <you@example.com>
2017-06-24 02:43:54 +0900

    Create three kinds of reusable ProgressBarComponent.

---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** 10%progress */
+export default function example10Percent() {
+  return <ProgressBar percent={10} width={150} />
+} 
