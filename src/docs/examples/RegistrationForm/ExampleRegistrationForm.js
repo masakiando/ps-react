16ea7c459d57af7a0ea4c800198fe5fdcf140881
Your Name <you@example.com>
2017-06-24 19:58:33 +0900

    Create a reusable RegistrationForm organisms Component.

---
+++ src/docs/examples/RegistrationForm/ExampleRegistrationForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RegistrationForm from 'ps-react/RegistrationForm';
+
+export default class ExampleRegistrationForm extends React.Component {
+  onSubmit = (user) => {
+    console.log(user);
+  }
+
+  render() {
+    return (
+      <RegistrationForm
+        onSubmit={this.onSubmit}
+      />
+    );
+  }
+}
