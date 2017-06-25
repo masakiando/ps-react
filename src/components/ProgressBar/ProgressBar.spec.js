fc37a1804898df0a9a0f2b162f11bbcf9afeea27
Your Name <you@example.com>
2017-06-25 20:41:50 +0900

    enzyme: shallow　Unit Testing:: ProgressBar

---
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ProgressBar from './ProgressBar';
+
+describe('ProgressBar', () => {
+  test(`getWidthAsPercentOfTotalWidth should
+        return 250 with total width of 500 and
+        percent of 50`, () => {
+
+      const wrapper = shallow(
+        <ProgressBar
+          percent={50}
+          width={500}
+          />
+      );
+
+      const width = wrapper.instance()
+      .getWidthAsPercentOfTotalWidth();
+
+      // const inst = wrapper.instance();
+      // console.log(inst);
+
+      expect(width).toEqual(250);
+  });
+});
