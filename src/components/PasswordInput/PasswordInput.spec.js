643d4731e044c5dec02719653ed2617e1144f4b3
Your Name <you@example.com>
2017-06-26 18:54:37 +0900

    Unit Testing PasswordInput with Snapshot.　 Add two tests.

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import PasswordInput from './PassWordInput';
+import PasswordInput from './PasswordInput';
+import {shallow} from 'enzyme';
 
 test('hides password quality by default', () => {
-  const tree = renderer.create(
-    <PasswordInput
-      htmlId="test"
-      name="test"
-      onChange={() => {}}
-      value="Uisi38#8iad"
-    />).toJSON();
+  const tree = renderer.create(<PasswordInput
+    htmlId="test"
+    name="test"
+    onChange={() => {}}
+    value="Uisi38#8iad" />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('shows password quality when enabled and a password is entered', () => {
+  const tree = renderer.create(<PasswordInput
+    htmlId="test"
+    name="test"
+    onChange={() => {}}
+    showQuality
+    value="Uisi38#8iad" />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('hides password quality when enabled but no password is entered', () => {
+  const tree = renderer.create(<PasswordInput
+    htmlId="test"
+    name="test"
+    onChange={() => {}}
+    showQuality
+    value="" />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
